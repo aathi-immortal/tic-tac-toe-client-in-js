7f9cc06b17af7d6cec456d5a61cf6be35903b5b1
aathi-immortal <89212658+aathi-immortal@users.noreply.github.com>
2023-09-12 23:08:30 +0530

    Update GameController.java

--- src/main/java/com/ticserver/server/controller/GameController.java
+++ src/main/java/com/ticserver/server/controller/GameController.java
@@ -28,7 +28,7 @@ public class GameController {
 
     public void recvivedGameState(@Payload Message message) {
         int roomId = message.getRoomId();
-        String destination = "/private/" + roomId;
+        String destination = "/private/123" ;
         System.out.println(destination);
         template.convertAndSend(destination, message);
 
